2bb585847f17e4734f27f6cc8b871e470c431378
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-06-22 23:01:05 +0530

    project section

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -1,21 +1,35 @@
+import React from "react";
 
-function ProjectCard({image,title,desc,techStack}) {
+function ProjectCard({ image, title, desc, tags }) {
   return (
-    <div>
-        <div></div>
-        <div>
-            <div>
-                {title}
-            </div>
-            <div>
-                {desc}
-            </div>
-            <div>
-                
-            </div>
+    <div className="flex flex-col md:flex-row gap-8 py-10">
+      {/* Image Section */}
+      <div className="max-w-[400px]">
+        <img
+          src={image}
+          alt={title}
+          className="rounded-lg grayscale hover:grayscale-0 transition duration-300 w-full"
+        />
+      </div>
+
+      {/* Text + Tags */}
+      <div className="flex flex-col justify-center max-w-xl">
+        <div className="text-2xl font-bold mb-3">{title}</div>
+        <div className="text-gray-300 mb-4">{desc}</div>
+        <div className="flex flex-wrap gap-2">
+          {Array.isArray(tags) &&
+            tags.map((tech, index) => (
+              <span
+                key={index}
+                className="bg-black text-yellow-300 px-4 py-1 rounded-full text-sm font-semibold"
+              >
+                {tech}
+              </span>
+            ))}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- portfolio/src/components/ProjectSection.jsx
+++ portfolio/src/components/ProjectSection.jsx
@@ -1,25 +1,27 @@
-import ProjectCard from "./ProjectCard"
-import projectData from "../data/ProjectData"
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import projectData from "../data/ProjectData";
+
 function ProjectSection() {
   return (
     <div className="my-30 px-[80px]">
-        <div className="font-montserrat font-semibold text-[38px]">
-            Project
+      <div className="font-montserrat font-semibold text-[38px] mb-10">
+        Project
+      </div>
+
+      {projectData.map((project, index) => (
+        <div key={index}>
+          <hr className="text-[rgb(253,202,34)] mb-10" />
+          <ProjectCard
+            image={project.image}
+            title={project.title}
+            desc={project.desc}
+            tags={project.tags}
+          />
         </div>
-        {projectData.map((project, index) => (
-        <>
-        <hr className="text-[rgb(253,202,34)]"/>
-        <ProjectCard
-          key={index}
-          image={project.image}
-          title={project.title}
-          description={project.description}
-          tags={project.tags}
-        /></>
       ))}
-        <ProjectCard/>
     </div>
-  )
+  );
 }
 
-export default ProjectSection
+export default ProjectSection;
