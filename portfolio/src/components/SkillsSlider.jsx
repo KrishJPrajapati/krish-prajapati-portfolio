cde33648bdea78a33907f30961a4e1e8d4a2e59b
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-06-22 11:14:37 +0530

    resolved

--- portfolio/src/components/SkillsSlider.jsx
+++ portfolio/src/components/SkillsSlider.jsx
@@ -1,4 +1,4 @@
-const skills=[{photo: src='',name:''}]
+// const skills=[{photo: src='',name:''}]
 export default function SkillsSlider(){
     return(
         <div className="md:p-10 bg-yellow-400">
